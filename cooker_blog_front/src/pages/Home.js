704a3531997e707767a1445e9553edb9e97354ce
Dragon1207 <smileboth0805@outlook.com>
2021-03-30 23:00:23 +0200

    add card home

--- cooker_blog_front/src/pages/Home.js
+++ cooker_blog_front/src/pages/Home.js
@@ -1,79 +1,71 @@
 import React, {useEffect, useState} from "react";
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import {Button, Container} from "@material-ui/core";
-import {Link, useLocation} from "react-router-dom";
 import axiosInstance from "../axios";
-import Alert from "@material-ui/lab/Alert";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import { red } from '@material-ui/core/colors';
+import dateFormat from 'dateformat';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 345,
+        ...theme.typography.button,
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(1),
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+}));
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
-    const location = useLocation();
 
     useEffect( () => {
         (async function getPosts() {
             const res = await axiosInstance.get('blog/posts/');
             if(res.status === 200) {
-                setPosts(res.data.sort((x, y) => y.published - x.published).slice(0, 3));
+                setPosts(res.data.sort((x, y) => y.published - x.published).slice(0, 4));
             }
         })()
     }, [])
-
-    const StyledTableCell = withStyles((theme) => ({
-        head: {
-            backgroundColor: theme.palette.common.black,
-            color: theme.palette.common.white,
-        },
-        body: {
-            fontSize: 14,
-        },
-    }))(TableCell);
-
-    const StyledTableRow = withStyles((theme) => ({
-        root: {
-            '&:nth-of-type(odd)': {
-                backgroundColor: theme.palette.action.hover,
-            },
-        },
-    }))(TableRow);
     
-    return (<>
-            <Container maxWidth="lg">
-            <h1>Lister les postes</h1>
-            {location.state !== undefined && location.state.success &&
-                <Alert severity="success" style={{marginBottom: '20px'}}>
-                    {location.state.success}
-                </Alert>
-            }
-            <TableContainer component={Paper}>
-                <Table aria-label="customized table">
-                    <TableHead>
-                        <StyledTableRow>
-                            <StyledTableCell>Titre</StyledTableCell>
-                            <StyledTableCell>Status</StyledTableCell>
-                        </StyledTableRow>
-                    </TableHead>
-                    <TableBody>
-                        {posts.map((post) => (
-                            <StyledTableRow key={post.title}>
-                                <StyledTableCell component="th" scope="row">
-                                    {post.title}
-                                </StyledTableCell>
-                                <StyledTableCell component="th" scope="row">
-                                    {post.status} - {post.published}
-                                </StyledTableCell>
-                            </StyledTableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+    const classes = useStyles();
+
+    return (
+        <Container>
+            <Box display="flex" style={{ marginTop: '10%' }}>
+            {posts.map((post) => (
+                <Card className={classes.root} style={{ margin: 'auto' }}>
+                <CardHeader
+                    avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        R
+                    </Avatar>
+                    }
+                    title={post.title}
+                    subheader={post.published}
+                />
+                <CardMedia
+                    className={classes.media}
+                    image="https://picsum.photos/200/300"
+                    title={post.title}
+                />
+                <CardContent style={{ textAlign: 'center' }}>
+                <Button size="small" variant="outlined" color="primary">Voir plus</Button>
+                </CardContent>
+                </Card>
+            ))}
+            </Box>
         </Container>
-        </>
     );
 }
