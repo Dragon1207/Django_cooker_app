86db98ff60931206147019631a94204ff801d97b
Dragon1207 <smileboth0805@outlook.com>
2021-04-01 15:54:54 +0200

    End test login

--- cooker_blog_front/cypress/integration/login.js
+++ cooker_blog_front/cypress/integration/login.js
@@ -1,8 +1,9 @@
 describe('Login', () => {
     it('Fill form & ', () => {
         cy.visit('/login')
-        cy.get('input[name="email"]').type('alexis.brohan@ynov.com')
-        cy.get('input[name="password"]').type('password')
-        cy.get('form').submit()
+        cy.get('input[name="email"]').type('alexis.brohan@ynov.com');
+        cy.get('input[name="password"]').type('password');
+        cy.get('.login-submit').click();
+        cy.get('h2').contains('Liste des recettes postés récemment');
     })
 })
